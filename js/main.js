0d0c12c711518da8b44c74c5dc239e1ab327284f
an-yc <43774821+an-yc@users.noreply.github.com>
2022-04-02 23:20:45 +0800

    2
    2

--- js/main.js
+++ js/main.js
@@ -75,3 +75,28 @@ $(function() {
   sr.reveal('.featured-projects', { viewFactor: 0.1 });
   sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
+
+const tabs = document.querySelector('.tabs');
+
+tabs.addEventListener('click', e => handleClick(e));
+
+function handleClick(e) {
+  const target = e.target;
+  const tabNum = target.dataset.tab;
+  const activeTab = document.querySelector('.tabs .active');
+  const activeContent = document.querySelector(
+    '.content .visible'
+  );
+  const currentContent = document.querySelector(
+    `.content__section[data-tab='${tabNum}']`
+  );
+
+  if (!tabNum) {
+    return;
+  }
+
+  activeTab.classList.remove('active');
+  target.classList.add('active');
+  activeContent.classList.remove('visible');
+  currentContent.classList.add('visible');
+}
